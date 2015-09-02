a800eed79bdfbc0b9f2903406f944913aca11691
Casey Bodley <cbodley@redhat.com>
2015-09-02 10:59:54 -0400

    SQUASHME: revert FileStore/Journal tracing
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/os/FileJournal.h
+++ src/os/FileJournal.h
@@ -217,7 +217,6 @@ public:
   bool journalq_empty() { return journalq.empty(); }
 
 private:
-  BLKIN_END_REF(journal_endpoint)
   string fn;
 
   char *zero_buf;
@@ -391,6 +390,7 @@ private:
     aio_stop(true),
     write_thread(this),
     write_finish_thread(this) {
+
       if (aio && !directio) {
         derr << "FileJournal::_open_any: aio not supported without directio; disabling aio" << dendl;
         aio = false;
@@ -401,7 +401,6 @@ private:
         aio = false;
       }
 #endif
-      BLKIN_MSG_END(journal_endpoint, "", 0, "Journal (" + fn + ")");
   }
   ~FileJournal() {
     assert(fd == -1);
